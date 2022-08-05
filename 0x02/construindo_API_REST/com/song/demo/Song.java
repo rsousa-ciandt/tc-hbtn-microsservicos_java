9cb23ba617c87d239e2a131331ebacdf21eb9450
rsousa <rsousa@ciandt.com>
2022-08-05 09:41:11 -0300

    refactor: lombok removed to add manually getters, setters, equals and hashcode to pass in test

--- 0x02/construindo_API_REST/com/song/demo/Song.java
+++ 0x02/construindo_API_REST/com/song/demo/Song.java
@@ -1,14 +1,64 @@
 package com.song.demo;
 
-import lombok.Data;
-import lombok.EqualsAndHashCode;
+import java.util.Objects;
 
-@Data
-@EqualsAndHashCode
 public class Song {
     private Integer id;
     private String nome;
     private String album;
     private String artista;
     private String anoLancamento;
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        Song song = (Song) o;
+        return Objects.equals(id, song.id) && Objects.equals(nome, song.nome) && Objects.equals(album, song.album) && Objects.equals(artista, song.artista) && Objects.equals(anoLancamento, song.anoLancamento);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(id, nome, album, artista, anoLancamento);
+    }
+
+    public Integer getId() {
+        return id;
+    }
+
+    public void setId(Integer id) {
+        this.id = id;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public String getAlbum() {
+        return album;
+    }
+
+    public void setAlbum(String album) {
+        this.album = album;
+    }
+
+    public String getArtista() {
+        return artista;
+    }
+
+    public void setArtista(String artista) {
+        this.artista = artista;
+    }
+
+    public String getAnoLancamento() {
+        return anoLancamento;
+    }
+
+    public void setAnoLancamento(String anoLancamento) {
+        this.anoLancamento = anoLancamento;
+    }
 }
